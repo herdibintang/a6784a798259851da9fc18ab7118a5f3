98dc7162a34694809f348ad21d60717010691ab3
Herdi Bintang <herdibintang@gmail.com>
2020-11-07 23:16:13 +0700

    follow phpcs

--- server/index.php
+++ server/index.php
@@ -13,63 +13,77 @@ use \Firebase\JWT\JWT;
 
 $capsule = new Capsule;
 
-$capsule->addConnection([
-  'driver'    => 'pgsql',
-  'host'      => $_ENV['DB_HOST'],
-  'database'  => $_ENV['DB_DATABASE'],
-  'username'  => $_ENV['DB_USERNAME'],
-  'password'  => $_ENV['DB_PASSWORD'],
-  'charset'   => 'utf8',
-  'collation' => 'utf8_unicode_ci',
-  'prefix'    => '',
-]);
+$capsule->addConnection(
+    [
+        'driver'    => 'pgsql',
+        'host'      => $_ENV['DB_HOST'],
+        'database'  => $_ENV['DB_DATABASE'],
+        'username'  => $_ENV['DB_USERNAME'],
+        'password'  => $_ENV['DB_PASSWORD'],
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ]
+);
 $capsule->setAsGlobal();
 
 $router = new RouteCollector();
 
-$router->post('/user', function () {
-  $requestBody = json_decode(file_get_contents('php://input'));
+$router->post(
+    '/user',
+    function () {
+        $requestBody = json_decode(file_get_contents('php://input'));
 
-  $hash = password_hash($requestBody->password, PASSWORD_BCRYPT);
+        $hash = password_hash($requestBody->password, PASSWORD_BCRYPT);
 
-  Capsule::table('users')->insert([
-    'email' => $requestBody->email,
-    'password' => $hash,
-  ]);
+        Capsule::table('users')->insert(
+            [
+                'email' => $requestBody->email,
+                'password' => $hash,
+            ]
+        );
 
-  echo json_encode([
-    'message' => 'User created'
-  ]);
+        echo json_encode(
+            [
+                'message' => 'User created'
+            ]
+        );
 
-  return;
-});
+        return;
+    }
+);
 
-$router->post('/user/login', function () {
-  $requestBody = json_decode(file_get_contents('php://input'));
+$router->post(
+    '/user/login',
+    function () {
+        $requestBody = json_decode(file_get_contents('php://input'));
 
-  $res = Capsule::table('users')
-    ->where('email', $requestBody->email)
-    ->first();
+        $res = Capsule::table('users')
+            ->where('email', $requestBody->email)
+            ->first();
 
-  if (!$res) {
-    http_response_code(404);
-    return;
-  }
+        if (!$res) {
+            http_response_code(404);
+            return;
+        }
 
-  if (!password_verify($requestBody->password, $res->password)) {
-    http_response_code(400);
-    return;
-  }
+        if (!password_verify($requestBody->password, $res->password)) {
+            http_response_code(400);
+            return;
+        }
 
-  $jwt = JWT::encode([], 'test');
+        $jwt = JWT::encode([], 'test');
 
-  echo json_encode([
-    'message' => 'Token created',
-    'token' => $jwt
-  ]);
+        echo json_encode(
+            [
+                'message' => 'Token created',
+                'token' => $jwt
+            ]
+        );
 
-  return;
-});
+        return;
+    }
+);
 
 
 $connection = new AMQPStreamConnection($_ENV['QUEUE_HOST'], 5672, 'guest', 'guest');
@@ -77,50 +91,55 @@ $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
 
-$router->post('/email/send', function () use ($channel, $connection) {
-  $headers = getallheaders();
+$router->post(
+    '/email/send',
+    function () use ($channel, $connection) {
+        $headers = getallheaders();
 
-  if (empty($headers['Authorization'])) {
-    http_response_code(401);
-    return;
-  }
+        if (empty($headers['Authorization'])) {
+            http_response_code(401);
+            return;
+        }
 
-  $token = '';
-  if (preg_match('/Bearer\s(\S+)/', $headers['Authorization'], $matches)) {
-    $token = $matches[1];
-  }
+        $token = '';
+        if (preg_match('/Bearer\s(\S+)/', $headers['Authorization'], $matches)) {
+            $token = $matches[1];
+        }
 
-  try {
-    $decoded = JWT::decode($token, 'test', array('HS256'));
-  } catch (\Exception $e) {
-    http_response_code(401);
-    return;
-  }
-  
-  $requestBody = file_get_contents('php://input');
+        try {
+            $decoded = JWT::decode($token, 'test', array('HS256'));
+        } catch (\Exception $e) {
+            http_response_code(401);
+            return;
+        }
 
-  $msg = new AMQPMessage($requestBody);
+        $requestBody = file_get_contents('php://input');
 
-  $channel->basic_publish($msg, '', 'hello');
+        $msg = new AMQPMessage($requestBody);
 
-  $channel->close();
-  $connection->close();
+        $channel->basic_publish($msg, '', 'hello');
 
-  echo json_encode([
-    "message" => "Email request added to queue"
-  ]);
+        $channel->close();
+        $connection->close();
 
-  return;
-});
+        echo json_encode(
+            [
+                "message" => "Email request added to queue"
+            ]
+        );
+
+        return;
+    }
+);
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
 
 try {
-  $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 } catch (HttpRouteNotFoundException $e) {
-  http_response_code(404);
-  echo $e->getMessage();
+    http_response_code(404);
+    echo $e->getMessage();
 } catch (HttpMethodNotAllowedException $e) {
-  http_response_code(405);
-  echo $e->getMessage();
+    http_response_code(405);
+    echo $e->getMessage();
 }
--- worker/index.php
+++ worker/index.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
@@ -13,53 +12,55 @@ $channel->queue_declare('hello', false, false, false, false);
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
 $callback = function ($msg) {
-  echo ' [x] Received ', $msg->body, "\n";
+    echo ' [x] Received ', $msg->body, "\n";
 
-  $requestBody = json_decode($msg->body);
+    $requestBody = json_decode($msg->body);
   
-  // Create the Transport
-  $transport = (new Swift_SmtpTransport($_ENV['MAIL_HOST'], $_ENV['MAIL_PORT']))
-    ->setUsername($_ENV['MAIL_USERNAME'])
-    ->setPassword($_ENV['MAIL_PASSWORD'])
-  ;
+    // Create the Transport
+    $transport = (new Swift_SmtpTransport($_ENV['MAIL_HOST'], $_ENV['MAIL_PORT']))
+        ->setUsername($_ENV['MAIL_USERNAME'])
+        ->setPassword($_ENV['MAIL_PASSWORD']);
 
-  // Create the Mailer using your created Transport
-  $mailer = new Swift_Mailer($transport);
+    // Create the Mailer using your created Transport
+    $mailer = new Swift_Mailer($transport);
 
-  // Create a message
-  $message = (new Swift_Message($requestBody->subject))
-    ->setFrom($requestBody->from)
-    ->setTo($requestBody->to)
-    ->setBody($requestBody->body)
-    ;
+    // Create a message
+    $message = (new Swift_Message($requestBody->subject))
+        ->setFrom($requestBody->from)
+        ->setTo($requestBody->to)
+        ->setBody($requestBody->body);
 
-  // Send the message
-  $result = $mailer->send($message);
+    // Send the message
+    $result = $mailer->send($message);
 
-  if($result){
-    $capsule = new Capsule;
+    if ($result) {
+        $capsule = new Capsule;
 
-    $capsule->addConnection([
-        'driver'    => 'pgsql',
-        'host'      => $_ENV['DB_HOST'],
-        'database'  => $_ENV['DB_DATABASE'],
-        'username'  => $_ENV['DB_USERNAME'],
-        'password'  => $_ENV['DB_PASSWORD'],
-        'charset'   => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix'    => '',
-    ]);
-    $capsule->setAsGlobal();
+        $capsule->addConnection(
+            [
+                'driver'    => 'pgsql',
+                'host'      => $_ENV['DB_HOST'],
+                'database'  => $_ENV['DB_DATABASE'],
+                'username'  => $_ENV['DB_USERNAME'],
+                'password'  => $_ENV['DB_PASSWORD'],
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => '',
+            ]
+        );
+        $capsule->setAsGlobal();
 
-    Capsule::table('emails')->insert([
-      'from' => $requestBody->from,
-      'to' => $requestBody->to,
-      'subject' => $requestBody->subject,
-      'body' => $requestBody->body
-    ]);
-  }
+        Capsule::table('emails')->insert(
+            [
+              'from' => $requestBody->from,
+              'to' => $requestBody->to,
+              'subject' => $requestBody->subject,
+              'body' => $requestBody->body
+            ]
+        );
+    }
 
-  echo " [x] Done\n";
+    echo " [x] Done\n";
 };
 
 $channel->basic_consume('hello', '', false, true, false, false, $callback);
